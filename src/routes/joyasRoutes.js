a87b48b0f8109286aa315acb814891b0939a51ee
Sherydan <tobar.luis.c@gmail.com>
2023-01-14 02:22:34 -0300

    added not found endpoint route

--- src/routes/joyasRoutes.js
+++ src/routes/joyasRoutes.js
@@ -6,5 +6,8 @@ const { reportRequest } = require("../middlewares/logger")
 
 router.get("/joyas", reportRequest, getAllJoyas)
 router.get("/joyas/filters", reportRequest, getFilteredJoyas)
+router.all("*", (req, res) => {
+    res.status(404).send("Endpoint not found!")
+})
 
 module.exports = router
