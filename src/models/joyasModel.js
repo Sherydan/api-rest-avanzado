c248c4ef34c6b93cd7d74057357bfb25e478bf06
Sherydan <tobar.luis.c@gmail.com>
2023-01-13 23:34:23 -0300

    add: added try catch statement on getJoyas function from joyasModel

--- src/models/joyasModel.js
+++ src/models/joyasModel.js
@@ -2,21 +2,25 @@ const pool = require("../helpers/connectionDb").getInstance();
 const format = require("pg-format");
 
 const getJoyas = async ({ limits = 10, order_BY = "id_ASC", page = 0 }) => {
-    const [campo, direccion] = order_BY.split("_");
-    const offset = (page - 1) * limits;
-    const formattedQuery = format(
-        "SELECT * FROM inventory ORDER BY %s %s LIMIT %s OFFSET %s",
-        campo,
-        direccion,
-        limits,
-        offset
-    );
+    try {
+        const [campo, direccion] = order_BY.split("_");
+        const offset = (page - 1) * limits;
+        const formattedQuery = format(
+            "SELECT * FROM inventory ORDER BY %s %s LIMIT %s OFFSET %s",
+            campo,
+            direccion,
+            limits,
+            offset
+        );
 
-    console.log(formattedQuery)
+        const { rows: joyas } = await pool.query(formattedQuery);
 
-    const {rows: joyas} = await pool.query(formattedQuery)
+        return joyas;
 
-    return joyas
+    } catch (error) {
+        console.log("Error while getting joyas", error.code, error.message);
+        return error;
+    }
 };
 
-module.exports = {getJoyas}
+module.exports = { getJoyas };
