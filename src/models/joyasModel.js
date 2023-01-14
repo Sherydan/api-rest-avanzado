954e9db33c4bfdb79f61fa5c29e15850ec68e901
Sherydan <tobar.luis.c@gmail.com>
2023-01-13 23:06:37 -0300

    added get joyas function on joyas model, added connectionDB file on helpers folder

---
+++ src/models/joyasModel.js
@@ -0,0 +1,18 @@
+const pool = require("../helpers/connectionDb").getInstance();
+const format = require("pg-format");
+
+const getJoyas = async ({ limits = 10, order_BY = "id_ASC", page = 0 }) => {
+    const [campo, direccion] = order_BY.split("_");
+    const offset = page * limits;
+    const formattedQuery = format(
+        "SELECT * FROM joyas ORDER BY %s %s LIMIT %s OFFSET %s",
+        campo,
+        direccion,
+        limits,
+        offset
+    );
+
+    const {rows: joyas} = await pool.query(formattedQuery)
+};
+
+module.exports = {getJoyas}
