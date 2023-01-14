d6a235aaedc8313fa87d4687670d8021ed45822a
Sherydan <tobar.luis.c@gmail.com>
2023-01-14 00:28:39 -0300

    added joyas/filters route

--- src/routes/joyasRoutes.js
+++ src/routes/joyasRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllJoyas } = require("../controllers/joyasController")
+const { getAllJoyas, getFilteredJoyas } = require("../controllers/joyasController")
 const { reportRequest } = require("../middlewares/logger")
 
 router.get("/joyas", reportRequest, getAllJoyas)
+router.get("/joyas/filters", reportRequest, getFilteredJoyas)
 
 module.exports = router
