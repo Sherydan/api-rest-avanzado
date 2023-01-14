624105741d20652aa24ed0c67d17001a5228b1c6
Sherydan <tobar.luis.c@gmail.com>
2023-01-14 01:53:02 -0300

    fix: getJoyasByFilters function on joyasModel

--- src/models/joyasModel.js
+++ src/models/joyasModel.js
@@ -12,7 +12,6 @@ const getJoyas = async ({ limits = 10, order_BY = "id_ASC", page = 0 }) => {
             limits,
             offset
         );
-        console.log(formattedQuery);
         const { rows: joyas } = await pool.query(formattedQuery);
 
         return joyas;
@@ -28,29 +27,32 @@ const getJoyasByFilters = async ({
     category,
     metal,
 }) => {
-    let filters = [];
-    const values = [];
-    const addFilter = (campo, comparador, valor) => {
-        values.push(valor);
-        const { length } = filters;
-        filters.push(`${campo} ${comparador} $${length + 1}`);
-    };
+    try {
+        let filters = [];
+        const values = [];
+        const addFilter = (campo, comparador, valor) => {
+            values.push(valor);
+            const { length } = filters;
+            filters.push(`${campo} ${comparador} $${length + 1}`);
+        };
 
-    if (precio_MAX) addFilter("price", "<=", precio_MAX);
-    if (precio_MIN) addFilter("price", ">=", precio_MIN);
-    if (category) addFilter("category", "=".category);
-    if (metal) addFilter("metal", "=", metal);
-    let consulta = "SELET * FROM inventory";
-    if (filters.length > 0) {
-        filters = filters.join(" AND ");
-        consulta = +` WHERE ${filters}`;
+        if (precio_MAX) addFilter("price", "<=", precio_MAX);
+        if (precio_MIN) addFilter("price", ">=", precio_MIN);
+        if (category) addFilter("category", "=", category);
+        if (metal) addFilter("metal", "=", metal);
+        let consulta = "SELECT * FROM inventory";
+        if (filters.length > 0) {
+            filters = filters.join(" AND ");
+            consulta += ` WHERE ${filters}`;
+        }
+
+        const { rows: joyas } = await pool.query(consulta, values);
+
+        return joyas;
+    } catch (error) {
+        console.log("Error while getting filtered joyas", error.code, error.message);
+        return error;
     }
-
-    console.log(consulta);
-
-    const { rows: joyas } = await pool.query(consulta, values);
-
-    return joyas;
 };
 
 module.exports = { getJoyas, getJoyasByFilters };
