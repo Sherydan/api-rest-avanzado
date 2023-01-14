af07ff92c884451d8d3ce3c3ac86b8cde0c7840e
Sherydan <tobar.luis.c@gmail.com>
2023-01-14 00:23:04 -0300

    added getJoyasByFilters function on joyasModel

--- src/models/joyasModel.js
+++ src/models/joyasModel.js
@@ -12,15 +12,45 @@ const getJoyas = async ({ limits = 10, order_BY = "id_ASC", page = 0 }) => {
             limits,
             offset
         );
-        console.log(formattedQuery)
+        console.log(formattedQuery);
         const { rows: joyas } = await pool.query(formattedQuery);
-        
-        return joyas;
 
+        return joyas;
     } catch (error) {
         console.log("Error while getting joyas", error.code, error.message);
         return error;
     }
+};
+
+const getJoyasByFilters = async ({
+    precio_MAX,
+    precio_MIN,
+    category,
+    metal,
+}) => {
+    let filters = [];
+    const values = [];
+    const addFilter = (campo, comparador, valor) => {
+        values.push(valor);
+        const { length } = filters;
+        filters.push(`${campo} ${comparador} $${length + 1}`);
+    };
+
+    if (precio_MAX) addFilter("price", "<=", precio_MAX);
+    if (precio_MIN) addFilter("price", ">=", precio_MIN);
+    if (category) addFilter("category", "=".category);
+    if (metal) addFilter("metal", "=", metal);
+    let consulta = "SELET * FROM inventory";
+    if (filters.length > 0) {
+        filters = filters.join(" AND ");
+        consulta = +` WHERE ${filters}`;
+    }
+
+    console.log(consulta);
+
+    const { rows: joyas } = await pool.query(consulta, values);
+
+    return joyas;
 };
 
 module.exports = { getJoyas };
