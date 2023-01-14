4fc41122ef5d2e023b38f2d35c1c729d4f6bcfdb
Sherydan <tobar.luis.c@gmail.com>
2023-01-13 23:27:48 -0300

    fix: chaned query on joyasModel file

--- src/models/joyasModel.js
+++ src/models/joyasModel.js
@@ -5,7 +5,7 @@ const getJoyas = async ({ limits = 10, order_BY = "id_ASC", page = 0 }) => {
     const [campo, direccion] = order_BY.split("_");
     const offset = page * limits;
     const formattedQuery = format(
-        "SELECT * FROM joyas ORDER BY %s %s LIMIT %s OFFSET %s",
+        "SELECT * FROM inventory ORDER BY %s %s LIMIT %s OFFSET %s",
         campo,
         direccion,
         limits,
